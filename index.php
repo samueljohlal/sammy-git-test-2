19fd2c8370df2500b5f15b265fa1ccbd0d56fc4e
samueljohlal <samueljlaluyan@gmail.com>
2023-04-23 21:50:47 +0700

    paragraf

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 </head>
 <body>
     <h1>Hello world</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis ipsum mollitia animi dignissimos consequuntur reprehenderit nostrum? Corrupti, voluptatum culpa provident corporis aperiam consequatur ut qui necessitatibus tenetur quidem laborum? Corrupti!</p>
 </body>
 </html>
